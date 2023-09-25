d95f6671eb3dd5de6b1114b835f4bd00e0e4d80a
Morgan Jones <morgan.jones@veygo.com>
2023-09-25 10:08:18 +0100

    Add flake8 for linting

--- scifinames/star_wars.py
+++ scifinames/star_wars.py
@@ -27,17 +27,22 @@ star_wars_data = {
     ]
 }
 
+
 def _title():
     return choice(star_wars_data["title"])
+
 
 def _first_name():
     return choice(star_wars_data["first_name"])
 
+
 def _second_name_prefix():
     return choice([" ", "-"])
 
+
 def _second_name():
     return choice(star_wars_data["second_name"])
+
 
 def star_wars_name():
     return _title() + _first_name() + _second_name_prefix() + _second_name()
